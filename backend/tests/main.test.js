7298c92ae7780d76640cafb43b280e07bd7d6c1b
Matt <mateuscaxambu@gmail.com>
2024-10-17 11:19:57 -0300

    fix: restructured project

--- backend/tests/main.test.js
+++
@@ -1,8 +0,0 @@
-const request = require("supertest");
-const app = require("../main");
-
-describe("GET /", () => {
-  it("responds with Hello World!", (done) => {
-    request(app).get("/").expect("Hello World!", done);
-  });
-});
